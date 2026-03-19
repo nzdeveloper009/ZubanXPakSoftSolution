6202f3470a386003b28a203bc0a8b9fd4243a5e6
Nokhaiz <nzdeveloper009@gmail.com>
2026-03-19 17:54:59 +0500

    feat(data): add ZubanDatabase with 4 entities and TypeConverters

---
+++ app/src/main/java/com/android/zubanx/data/local/db/ZubanDatabase.kt
@@ -0,0 +1,26 @@
+package com.android.zubanx.data.local.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.android.zubanx.data.local.db.converter.ListStringConverter
+import com.android.zubanx.data.local.db.dao.*
+import com.android.zubanx.data.local.db.entity.*
+
+@Database(
+    entities = [
+        TranslationEntity::class,
+        FavouriteEntity::class,
+        DictionaryEntity::class,
+        OfflineLanguagePackEntity::class
+    ],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(ListStringConverter::class)
+abstract class ZubanDatabase : RoomDatabase() {
+    abstract fun translationDao(): TranslationDao
+    abstract fun favouriteDao(): FavouriteDao
+    abstract fun dictionaryDao(): DictionaryDao
+    abstract fun offlineLanguagePackDao(): OfflineLanguagePackDao
+}
